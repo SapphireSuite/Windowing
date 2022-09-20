fb3d47cc48741c06c18f72f3f0c52c3d505a4bc4
mrouffet <maximerouffet@gmail.com>
2022-09-20 15:06:50 +0900

    [Add] GLFW Window impl

---
+++ Include/SA/Windowing/GLFW/GLFWAPI.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef SAPPHIRE_WINDOWING_GLFW_API_GUARD
+#define SAPPHIRE_WINDOWING_GLFW_API_GUARD
+
+/**
+*	\file GLFW.hpp
+*
+*	\brief \b GLFW include support file.
+*
+*	\ingroup GLFW
+*	\{
+*/
+
+#include <GLFW/glfw3.h>
+
+/** \} */
+
+#endif // SAPPHIRE_WINDOWING_GLFW_API_GUARD
