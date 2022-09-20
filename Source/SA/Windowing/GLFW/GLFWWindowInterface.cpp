8c4cf3eb0d3fec74872dd731f2fbb379add3e6d2
mrouffet <maximerouffet@gmail.com>
2022-09-20 14:45:25 +0900

    [Add] GLFW interface init/terminate

--- Source/SA/Windowing/GLFW/GLFWWindowInterface.cpp
+++ Source/SA/Windowing/GLFW/GLFWWindowInterface.cpp
@@ -8,13 +8,24 @@ namespace SA
 {
 	namespace GLFW
 	{
+		void ErrorCallback(int32_t error, const char* description)
+		{
+			SA_LOG(L"GLFW Error [" << error << L"]:" << description, Error, SA/Windowing/GLFW);
+		}
+
 		void WindowInterface::Create()
 		{
+			glfwSetErrorCallback(ErrorCallback);
+
+			SA_ASSERT_EXEC(Default, SA/Windowing/GLFW, glfwInit(), L"GLFW init failed!");
+
 			SA_LOG(L"Window Interface created.", Infos, SA/Windowing/GLFW);
 		}
 
 		void WindowInterface::Destroy()
 		{
+			glfwTerminate();
+
 			SA_LOG(L"Window Interface destroyed.", Infos, SA/Windowing/GLFW);
 		}
 	}
